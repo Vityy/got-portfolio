a45b866cc0644bec47cbd6f8b7e0f8fe4c62273b
Vityy <ygensoftvd@gmail.com>
2025-12-11 09:48:03 +0100

    Added detect changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CharactersList } from "./components/characters-list/characters-list";
+import { Character } from './shared/service/character';
+import { Characters } from './shared/models/characters.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CharactersList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('got-portfolio');
+export class App implements OnInit {
+  // On injecte le servec précédemment créé.
+  private characterService = inject(Character);
+  private cdr = inject(ChangeDetectorRef);
+
+  // On stock tous les personnages dans un tableau.
+  protected characters!: Characters[]; // ! est pour indiquer qu'on initialise à rien
+
+  protected stringTest: string = "Je suis une variable";
+
+  ngOnInit(): void {
+    this.characterService.getCharacters().subscribe((charactersFromApi: Characters[]) => {
+      this.characters = charactersFromApi;
+      this.cdr.detectChanges();
+    });
+  }
 }
---
+++ src/app/components/characters-list/characters-list.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Characters } from '../../shared/models/characters.model';
+
+@Component({
+  selector: 'app-characters-list',
+  imports: [],
+  templateUrl: './characters-list.html',
+  styleUrl: './characters-list.scss',
+})
+export class CharactersList {
+  @Input() charactersFromApi!: Characters[];
+  @Input() stringToShow!: string;
+}
