a3155bc3b319468122fb78e3b1780faf4c1ef326
Vityy <ygensoftvd@gmail.com>
2025-12-12 15:36:23 +0100

    Refacto Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
-  {path: 'toto', component: App}, // Eager loading - Pour les pages présentes directement, comme une homepage
+  {path: '', component: Home}, // Eager loading - Pour les pages présentes directement, comme une homepage
   {path: 'home', redirectTo: '', pathMatch: 'full'},
   {path: 'continents', loadComponent: () => import('./components/continents-list/continents-list')
     .then((component) => component.ContinentsList) // Lazy loading - charger dynamiquement le composant. Si la route n'est pas activée, le composant n'est pas chargé.
-  }
+  },
+  {path: 'countries', loadComponent: () => import('./components/countries/countries')
+    .then((component) => component.Countries)}
 ];
