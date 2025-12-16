97b16b17eb32188bc724f639a520c7ca56936dc5
Vityy <ygensoftvd@gmail.com>
2025-12-16 15:24:58 +0100

    Fixed api url calls

--- src/app/shared/services/character.ts
+++ src/app/shared/services/character.ts
@@ -2,15 +2,16 @@ import { Injectable, inject } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Characters} from '../models/characters.model';
 import {Observable} from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  private charactersUrl = 'https://thronesapi.com/api/v2/Characters';
+  private charactersUrl = environment.apiUrl;
   private httpClient = inject(HttpClient);
 
   public getCharacters() : Observable<Characters[]> {
-    return this.httpClient.get<Characters[]>(this.charactersUrl);
+    return this.httpClient.get<Characters[]>(this.charactersUrl + '/Characters');
   }
 }
